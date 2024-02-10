77e91cee0d30e5b6d883660e0a1b3b3fbfa80123
Mr.Minkov <martoer4@gmail.com>
2024-02-10 14:25:47 +0200

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,12 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {System.out.println("Hello Devi!");
+    public static void main(String[] args) {
+        calculateAVG(3.5,6.5,133);
     }
+    public static void calculateAVG (double number1, double number2, double number3){
+        double  average = (number1 + number2 + number3)/3;
+        System.out.println(average);
+    }
+
 }
