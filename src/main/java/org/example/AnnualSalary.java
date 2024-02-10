0219e88c3e06c3c1bf44bc4c5dfb8718d61a6b1a
Mr.Minkov <martoer4@gmail.com>
2024-02-10 15:49:26 +0200

    Final commit

---
+++ src/main/java/org/example/AnnualSalary.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class AnnualSalary {
+    public static void main(String[] args) {
+    salary(3000.33);
+    }
+
+    public static void salary (double monthlySalary){
+        double annualSalary = monthlySalary*12;
+        System.out.print(annualSalary);
+
+    }
+}
--- src/main/java/org/example/Average3Numbers.java
+++ src/main/java/org/example/Average3Numbers.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Average3Numbers {
     public static void main(String[] args) {
-        calculateAVG(3.5,6.5,133);
+
+        calculateAVG(3.5,6800.123456,133);
     }
     public static void calculateAVG (double number1, double number2, double number3){
         double  average = (number1 + number2 + number3)/3;
